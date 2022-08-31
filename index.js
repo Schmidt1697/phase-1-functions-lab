9028c51fd3f64fa5ad0fee201440b3dd0516a60c
Anna Schmidt <marshall1697@gmail.com>
2022-08-30 19:57:07 -0600

    commit tree 4c493979c5cafb3c0b854ff44c529b8dac7d1c8e

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    return Math.abs(pickUp - 42);
+    
+};
+
+function distanceFromHqInFeet(pickUp){
+   return Math.abs(pickUp - 42) * 264;
+};
+
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264;
+}
+
+// function calculatesFarePrice(start, end){
+//     let distance = Math.abs(start - end) * 264;
+//     let fee;
+//     if (distance < 400){
+//         fee = 0;
+//         return fee;
+
+//     }else if( distance >= 400 && distance < 2000){
+//         fee = (distance - 400) * 0.02;
+//         return fee;
+//     }else if( distance >= 2000 && distance < 2500){
+//         fee = 25;
+//         return fee;
+//     }else {
+//         return 'cannot travel that far'
+//     }
+// }
+
+
+//make new function inside of this function?
+//
+function calculatesFarePrice(start, end){
+    const distance = Math.abs(start - end) * 264;
+     let fee;
+
+        switch(true){
+            case distance < 400:
+               return fee = 0;
+                break;
+            case distance >= 400 && distance < 2000:
+                return fee = (distance - 400) * 0.02;
+                break;
+            case distance >= 2000 && distance < 2500:
+                return fee = 25;
+                break;
+            default: 
+                return 'cannot travel that far'
+        }
+   
+        // return fee
+
+    }
